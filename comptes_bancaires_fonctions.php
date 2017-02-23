90b1ba40c82cbdbf882021f767d3f56c9e7ab49e
abelass <rainer@websimple.be>
2017-02-23 15:38:37 +0100

    commentaire

--- comptes_bancaires_fonctions.php
+++ comptes_bancaires_fonctions.php
@@ -20,7 +20,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * @return array       Données du compte par défaut
  */
 function compte_bancaire_defaut($objet, $id_objet) {
-	// On cherche maintenant s'il existe une personnalisation pour les taxes : prix_<objet>() dans prix/<objet>.php
+	// On appele la fonction génñerique
 	$fonction_compte_defaut = charger_fonction('objet', 'compte_defaut');
 
 	return $fonction_compte_defaut($objet, $id_objet, $objet_parent, $id_objet_parent);
